b36a2e40ec4867da4adbb3e028fd11fc559db648
kyher <kyle150896@gmail.com>
2022-04-15 17:34:09 +0100

    Add further fixture files for cypress
    Added further fixture files to make tests cleaner.

--- cypress/integration/appDisplays.spec.js
+++ cypress/integration/appDisplays.spec.js
@@ -1,37 +1,48 @@
 /// <reference types="cypress" />
-import { links } from "../fixtures/links.json";
+import {
+  createWorkoutLinkText,
+  viewWorkoutLinkText,
+  aboutLinkText,
+  backButtonLinkText,
+} from "../fixtures/links.json";
+import { HomeLink, BackButton } from "../fixtures/testIds.json";
+import {
+  aboutHeading,
+  viewWorkoutHeading,
+  createWorkoutHeading,
+} from "../fixtures/headings.json";
 
 describe("Check that the app displays", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
   it("displays the three menu options on the home page", () => {
-    cy.get('[data-testid="HomeLink"]').should("have.length", 3);
-    cy.get('[data-testid="HomeLink"]').contains(links.createWorkoutLinkText);
-    cy.get('[data-testid="HomeLink"]').contains(links.viewWorkoutLinkText);
-    cy.get('[data-testid="HomeLink"]').contains(links.aboutLinkText);
+    cy.get(HomeLink).should("have.length", 3);
+    cy.get(HomeLink).contains(createWorkoutLinkText);
+    cy.get(HomeLink).contains(viewWorkoutLinkText);
+    cy.get(HomeLink).contains(aboutLinkText);
   });
   it("displays the about page", () => {
-    cy.get("h2").contains(links.aboutLinkText).click();
-    cy.get('[data-testid="BackButton"]').contains(links.backButtonLinkText);
-    cy.get("h1").contains("About Bootcamp Buddy");
+    cy.get("h2").contains(aboutLinkText).click();
+    cy.get(BackButton).contains(backButtonLinkText);
+    cy.get("h1").contains(aboutHeading);
     cy.get("p").contains(
       "A site to aid the creation of bootcamp workouts. Made using NextJS."
     );
   });
   it("displays the view page", () => {
     cy.clearLocalStorage();
-    cy.get("h2").contains(links.viewWorkoutLinkText).click();
-    cy.get('[data-testid="BackButton"]').contains(links.backButtonLinkText);
-    cy.get("h1").contains("View your planned workout below");
+    cy.get("h2").contains(viewWorkoutLinkText).click();
+    cy.get(BackButton).contains(backButtonLinkText);
+    cy.get("h1").contains(viewWorkoutHeading);
     cy.get("h3").contains("WARMUP");
     cy.get("h3").contains("EXERCISE");
     cy.get("h3").contains("STRETCH");
     cy.get("div").contains("No activity set.");
   });
   it("displays the create page", () => {
-    cy.get("h2").contains(links.createWorkoutLinkText).click();
-    cy.get('[data-testid="BackButton"]').contains(links.backButtonLinkText);
-    cy.get("h1").contains("Create a new workout");
+    cy.get("h2").contains(createWorkoutLinkText).click();
+    cy.get(BackButton).contains(backButtonLinkText);
+    cy.get("h1").contains(createWorkoutHeading);
   });
 });
